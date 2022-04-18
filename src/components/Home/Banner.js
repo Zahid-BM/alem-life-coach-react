8f9c46eebc4a0d1444d1775b5bf77c1cfd40c01b
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-18 07:47:50 +0600

    Alhamdulillah homepage checked and adjust some position and design after checking in firebase hosted link

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -4,13 +4,13 @@ import { Col, Container, Row } from 'react-bootstrap';
 const Banner = () => {
     return (
         <>
-            <Container className='my-5'>
-                <Row>
-                    <Col lg={6}>
-                        <img className='w-100 rounded-3' src="https://cdn.pixabay.com/photo/2019/08/14/09/53/consulting-4405260_960_720.jpg" alt="" />
+            <Container fluid className='my-5'>
+                <Row >
+                    <Col className='mt-4' lg={6}>
+                        <img className='w-100 rounded-3 h-100' src="https://cdn.pixabay.com/photo/2019/08/14/09/53/consulting-4405260_960_720.jpg" alt="" />
                     </Col>
                     <Col className='my-auto' lg={6}>
-                        <h1 className='fw-bolder display-2 '>Welcome to <span className='text-warning'>ALEM Life Coach</span></h1>
+                        <h1 className='fw-bolder display-3 '>Welcome to <span className='text-warning'>ALEM Life Coach</span></h1>
                         <p>I have been running this life coach website since 8 years successfully. Almost 3000 people have taken my life coach services and improved their lives tremendously and remarkably. Students, corporate and business individuals, housewives, doctors and programmers are also in close connection after having the courses. I do not sell courses rather I think I make a strong connection with people by improving their lives.</p>
                     </Col>
                 </Row>
--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -6,10 +6,10 @@ const Service = ({ service }) => {
     return (
         <>
             <Col className='g-4' lg={4}>
-                <Card className='h-100'>
-                    <Card.Img variant="top" src={img} />
-                    <Card.Body>
-                        <Card.Title className='fw-bold fs-3'>{name}</Card.Title>
+                <Card className='h-100 shadow'>
+                    <Card.Img className='img-thumbnail p-3' variant="top" src={img} />
+                    <Card.Body className='p-4'>
+                        <Card.Title className='fw-bold fs-3 mt-3'>{name}</Card.Title>
                         <Card.Subtitle className='text-muted fs-4 my-3'> Price : $ {price}</Card.Subtitle>
                         <Card.Text>
                             {description}
