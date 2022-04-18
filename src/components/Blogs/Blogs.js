165f2f8998045c13f2e0b5a21ca310a3a1bd1040
BM. Zahidul Islam <bmzahidulislam@outlook.com>
2022-04-18 17:39:29 +0600

    Alhamdulillah blogs part done with all answers

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -8,41 +8,23 @@ const Blogs = () => {
                 <h1 className='text-center text-info mt-5 mb-3'>Mandatory to Know the answers</h1>
                 <Row>
                     <Col className='my-auto py-5' lg={7}>
-                        <Accordion >
-                            <Accordion.Item className='bg-dark text-white' eventKey="0">
-                                <Accordion.Header >What is the coaching method?</Accordion.Header>
+                        <Accordion defaultActiveKey="0">
+                            <Accordion.Item className='bg-info text-white' eventKey="0">
+                                <Accordion.Header >What is the difference between authorization and authentication?</Accordion.Header>
                                 <Accordion.Body>
-                                    Basically all these are pre-recorded video content. But depending on the requirement I also do specific video for specific people for some cases. Alongside, live call coaching is available 2 days in a week.
+                                    Authentication is a process by which something/someone can be checked and verified the authenticity of identity or ownerships etc. And authorization is a process by which giving some access or ownership to certain things.
                                 </Accordion.Body>
                             </Accordion.Item>
-                            <Accordion.Item className='bg-dark text-white' eventKey="1">
-                                <Accordion.Header>How long does you provide services after the courses ?</Accordion.Header>
+                            <Accordion.Item className='bg-info text-white' eventKey="1">
+                                <Accordion.Header>Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
                                 <Accordion.Body>
-                                    I support and keep connection with my clients life long.
+                                    Firebase is famous for it's easier ways to use SDKs, provides backend services, rich ready made UI library and has almost all social media account authentication facilities. For all these reason I use firebase. There are other popular alternatives to firebase like Parse, Back4app, Kuzzle, Pubnub etc.
                                 </Accordion.Body>
                             </Accordion.Item>
-                            <Accordion.Item className='bg-dark text-white' eventKey="2">
-                                <Accordion.Header>Do you provide any discounts or coupons ?</Accordion.Header>
+                            <Accordion.Item className='bg-info text-white' eventKey="2">
+                                <Accordion.Header>What other services does firebase provide other than authentication ?</Accordion.Header>
                                 <Accordion.Body>
-                                    Yes all the year it is running to provide discounts and coupon codes. Alongside, special discount for struggling people.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item className='bg-dark text-white' eventKey="3">
-                                <Accordion.Header>Who run the course services alongside you?</Accordion.Header>
-                                <Accordion.Body>
-                                    All the course contents and coaching materials created and served by me only.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item className='bg-dark text-white' eventKey="4">
-                                <Accordion.Header>Can I get my money back if no improvement ?</Accordion.Header>
-                                <Accordion.Body>
-                                    Yes, sure. I offer 100% money back guarantee by 2 months if there is no improvements.
-                                </Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item className='bg-dark text-white' eventKey="5">
-                                <Accordion.Header>I have more questions. How to get answers? </Accordion.Header>
-                                <Accordion.Body>
-                                    You can can keep eyes on the FAQ section and if not satisfied then you can call and discuss me directly on Thursday 9pm over cellphone.
+                                    Firebase has become this much popular because it provides a varieties of services other than authentication. It provides Cloud Firestore, Cloud Functions, Hosting, Cloud Storage, Google Analytics, Predictions, Cloud Messaging and so on alongside authentication.
                                 </Accordion.Body>
                             </Accordion.Item>
                         </Accordion>
@@ -52,7 +34,6 @@ const Blogs = () => {
                     </Col>
                 </Row>
             </Container>
-
         </>
     );
 };
